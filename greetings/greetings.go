96d6d54b5264311e3c994da7590a67813e31c954
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-02-17 12:00:57 -0700

    initial commit, up to Hellos

---
+++ greetings/greetings.go
@@ -0,0 +1,55 @@
+package greetings
+
+import (
+    "errors"
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+    // If no name was given, return an error with a message.
+    if name == "" {
+        return name, errors.New("empty name")
+    }
+    // Create a message using a random format.
+    message := fmt.Sprintf(randomFormat(), name)
+    return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+    // a map to associate names with messages
+    messages := make(map[string]string)
+    // loop through slice of names calling hello function to get a message from each name
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        // in the map, associate the retrieved message with the name
+        messages[name] = message
+    }
+    return messages, nil
+}
+
+
+// init sets initial values for variables used in the function.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    // Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
+}
